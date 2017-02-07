b64ed54956dda4af07e7ae4196405c400fa54e5e
Kevin DeKorte <kdekorte@yahoo.com>
2017-02-07 09:08:07 -0700

    Basic electron working

--- index.js
+++ index.js
@@ -1,13 +1,60 @@
-var express = require('express')
-var app = express()
-var path = require('path');
+const {app, BrowserWindow} = require('electron')
+const path = require('path')
+const url = require('url')
 
-app.get('/', function (req, res) {
-  res.redirect("/static/index.html")
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let win
+
+function createWindow () {
+  // Create the browser window.
+  var kioskMode = false;
+  console.log(process.argv);
+  process.argv.forEach(function(argv){
+    if (argv == '--kiosk') {
+      kioskMode = true;
+    }
+  })
+  win = new BrowserWindow({width: 800, height: 480, kiosk: kioskMode})
+
+  // and load the index.html of the app.
+  win.loadURL(url.format({
+    pathname: path.join(__dirname, 'public', 'index.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
+
+// Open the DevTools.
+//  win.webContents.openDevTools()
+
+  // Emitted when the window is closed.
+  win.on('closed', () => {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    win = null
+  })
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
 })
 
-app.use('/static', express.static(path.join(__dirname, 'public')))
-
-app.listen(8000, function () {
-  console.log('Kevin\'s Weather Clock listening on port 8000!')
+app.on('activate', () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (win === null) {
+    createWindow()
+  }
 })
+
