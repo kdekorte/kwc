936c574284380f4903c9079c472e6726cf65ec58
Kevin DeKorte <kdekorte@gmail.com>
2019-08-02 10:41:53 -0600

    Create config.js
    Add missing file to code

---
+++ public/config.js
@@ -0,0 +1,13 @@
+var usegeoloc = false;
+var foundLat = 0;
+var foundLon = 0;
+var state = 'CO';
+var city = 'Fort Morgan';
+var country = 'US';
+// var cityid = '5577158';
+// get a key from https://openweathermap.org
+var owmappid = ''; 
+//var wuurlbase = ''; // 'http://api.wunderground.com/api/';
+var owmurlbase = 'http://api.openweathermap.org/data/2.5/';
+// https://ipstack.com
+var ipstackkey = ''; 
